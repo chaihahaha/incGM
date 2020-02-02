6231128afe7c39d69104bcd56d4839d4c7782eb4
柴士童 <836275709@qq.com>
2020-02-02 14:00:07 +0100

    Update incGM.py

--- incGM.py
+++ incGM.py
@@ -18,16 +18,25 @@ class MNI:
                 self.table[i][dic[i]] = 1
             self.supp[i] = len(self.table[i].values())
     def support(self):
-        return min(self.supp.values())
+        v = self.supp.values()
+        return min(v) if v else 0
     
 def neighbor(a,G):
     # neighbor of a in G
     return reduce(operator.add,[[(i,j) for j in G.neighbors(i) if j not in a.nodes] for i in a.nodes],[])
 
+def same(g1,g2):
+    return (g1.nodes == g2.nodes) and (g1.edges == g2.edges)
+def union(g1,g2):
+    g=g1.copy()
+    g.add_nodes_from(g2.nodes)
+    g.add_edges_from(g2.edges)
+    return g
+
 class FRINGE:
     def __init__(self):
-        self.MFS = set()
-        self.MIFS = set()
+        self.MFS = []
+        self.MIFS = []
         
 def EVALUATE(G, tau, pattern):
     sgs_nodes = permutations(G.nodes, k)
@@ -39,3 +48,31 @@ def EVALUATE(G, tau, pattern):
             mni.add(embedding.mapping)
     print(mni.table)
     return mni.support()>=tau
+
+def UPDATEFRINGE(fringe, S, isFreq, tau, G):
+    if isFreq:
+        exist = False
+        for i in fringe.MFS:
+            if same(i,S):
+                exist=True
+        if not exist:
+            print('1', S.edges)
+            fringe.MFS.append(S)
+
+        for i in fringe.MIFS:
+            if same(i,S):
+                print('2',i.edges)
+                fringe.MIFS.remove(i)
+                break
+        for i in fringe.MFS:
+            if len(i) == len(S) and not same(i,S):
+                u = union(i,S)
+                if not EVALUATE(G, tau,u):
+                    exist=False
+                    for j in fringe.MIFS:
+                        if same(j,u):
+                            exist=True
+                    if not exist:
+                        print('3',u.edges)
+                        fringe.MIFS.append(u)
+    return
