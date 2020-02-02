18fe7536724e4fec49bf2d5329c15746292efb03
柴士童 <836275709@qq.com>
2020-02-02 23:09:56 +0100

    Update incGM.py

--- incGM.py
+++ incGM.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from networkx.algorithms import isomorphism
-from itertools import permutations
+from itertools import permutations, combinations
 from functools import reduce
 import operator
 
@@ -10,11 +10,12 @@ def neighbor(a,G):
 
 def same(g1,g2):
     return (g1.nodes == g2.nodes) and (g1.edges == g2.edges)
-def union(g1,g2):
-    g=g1.copy()
-    g.add_nodes_from(g2.nodes)
+
+def union(g1,g2,G):
+    g=nx.Graph()
+    g.add_edges_from(g1.edges)
     g.add_edges_from(g2.edges)
-    return g
+    return G.edge_subgraph(g.edges)
 
 class FRINGE:
     def __init__(self):
@@ -38,33 +39,43 @@ class MNI:
     def support(self):
         v = self.supp.values()
         return min(v) if v else 0
+    
 def EVALUATE(G, tau, pattern):
-    k = len(pattern)
+    n_v = len(pattern)
+    n_e = pattern.size()
     pattern_nodes = list(pattern.nodes)
     sgs_nodes = []
     components = nx.connected_components(G)
     for i in components:
-        sgs_nodes.append(list(combinations(list(i),k)))
-    sgs_nodes = reduce(operator.add,sgs_nodes)
-    sgs =  [G.subgraph(i) for i in sgs_nodes]
+        sgs_nodes += list(combinations(list(i),n_v))
+    sgs = []
+    for i in sgs_nodes:
+        n_v_subsets = list(combinations(G.subgraph(i).edges,n_e))
+        for j in n_v_subsets:
+            n_e_subgraph = G.edge_subgraph(j)
+            if nx.is_connected(n_e_subgraph):
+                sgs.append(n_e_subgraph)
+    sgs_nodes = [i.nodes for i in sgs]
     mni = MNI(pattern)
+    count_iso = 0
     for i in range(len(sgs)):
         gm = isomorphism.GraphMatcher(pattern, sgs[i])
         is_iso = gm.is_isomorphic()
-        sg_perm = permutations(sgs_nodes[i])
         if is_iso:
-            for j in sg_perm:
-                embedding = dict(zip(pattern_nodes,j))
-                mni.add(embedding)
+            embedding = dict(zip(pattern_nodes,list(sgs_nodes[i])))
+            mni.add(embedding)
     return mni.support()>=tau
+
 def exist(u, arr):
     for j in arr:
         if same(j,u):
             return True
     return False
+
 def PRUNE(fringe, S):
     fringe.MFS = [i for i in fringe.MFS if not subset(i,S)]
     return
+
 def UPDATEFRINGE(fringe, S, isFreq, tau, G):
     # return # of deleted in MIFS
     count = 0
@@ -77,12 +88,14 @@ def UPDATEFRINGE(fringe, S, isFreq, tau, G):
                 fringe.MIFS.remove(i)
                 count += 1
                 break
-        for i in fringe.MFS:
-            if len(i) == len(S) and not same(i,S):
-                u = union(i,S)
-                if not exist(u,fringe.MIFS):
+        for i in range(len(fringe.MFS)):
+            MFSi = fringe.MFS[i]
+            if len(MFSi) == len(S) and not same(MFSi,S):
+                u = union(MFSi,S,G)
+                if not EVALUATE(G,tau,u) and not exist(u,fringe.MIFS):
                     fringe.MIFS.append(u)
     return count
+
 def subset(g1,g2):
     # return if g1 <= g2
     for i in g1.nodes:
