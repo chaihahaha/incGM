1f4b2700d76617b96c616fa184808e486c6d39a5
chaihahaha <836275709@qq.com>
2020-02-23 02:13:21 +0100

    my eval

--- incGM.py
+++ incGM.py
@@ -31,30 +31,29 @@ class FRINGE:
 class MNI_table:
     def __init__(self,S_nodes):
         self.nodes = S_nodes
-        self.table = dict()
+        self.table = dict() # lower bound of solution (marked solutions)
+        self.domain = dict() # upper bound of solution
         for i in self.nodes:
-            self.table[i]=dict()
-        self.supp = dict()
+            self.table[i]=set()
     def add(self, dic):
         for i in self.nodes:
-            if dic[i] in self.table[i].keys():
-                self.table[i][dic[i]] += 1
-            else:
-                self.table[i][dic[i]] = 1
-            self.supp[i] = len(self.table[i].values())
-    def support(self):
-        v = self.supp.values()
-        return min(v) if v else 0
+            self.table[i].add(dic[i])
     def frequent(self, tau):
-        return self.support()>=tau
+        minsupp = min([len(self.table[i]) for i in self.nodes])
+        return minsupp>=tau
+    def infrequent(self, tau):
+        maxsupp = min([len(self.domain[i]) for i in self.nodes])
+        return maxsupp<tau
+
 
 class FELS:
     def __init__(self,S_nodes):
         self.S_nodes = S_nodes
         self.mni = MNI_table(self.S_nodes)
-        self.embeddings = []
+        self.embeddings = set()
         self.inverted_index = dict() # dict of set of node sets(subgraph)
     def add(self, embedding):
+        self.embeddings.add(frozenset(embedding.values()))
         self.mni.add(embedding)
     def add_inverted(self, embedding):
         emb_nodes = frozenset(embedding.keys())
@@ -83,16 +82,42 @@ class FELS_dict:
         self.elem(subG).add_inverted(S2G_embedding)
         return
 
+    def remove(self, S_nodes, v, u):
+        self.domain(S_nodes)[v] -= {u}
+        return
+
     def keys(self):
         return self.elements.keys()
 
     def elem(self, S_nodes):
         return self.elements[S_nodes]
 
+    def mni(self, S_nodes):
+        return self.elements[S_nodes].mni.table
+
+    def domain(self, S_nodes):
+        return self.elements[S_nodes].mni.domain
+
+    def intersection(self, S_nodes, ds, G):
+        if S_nodes not in self.keys():
+            self.elements[S_nodes] = FELS(S_nodes)
+        for i in S_nodes:
+            inter = frozenset(G)
+            for j in ds:
+                if i in j and j in self.keys():
+                    inter &= frozenset(self.mni(j)[i])
+            self.domain(S_nodes)[i] = inter - frozenset(self.mni(S_nodes)[i])
+        return
+
     def is_frequent(self, S_nodes, tau,G):
         if not nx.is_connected(G.subgraph(S_nodes)):
             return False
         return self.elem(S_nodes).mni.frequent(tau)
+
+    def is_infrequent(self, S_nodes, tau):
+        if not nx.is_connected(G.subgraph(S_nodes)):
+            return True
+        return self.elem(S_nodes).mni.infrequent(tau)
 
     def iso_graphs(self, S_nodes):
         gs = set()
@@ -102,6 +127,12 @@ class FELS_dict:
                 gs.add(j)
         return gs
 
+def direct_subgraphs(G, S_nodes):
+    ds = []
+    for i in S_nodes:
+        if nx.is_connected(G.subgraph(S_nodes - {i})):
+            ds.append(S_nodes - {i})
+    return ds
 
 def SEARCHLIMITED(S_nodes,search_region,G):
     embeddings = False
@@ -120,17 +151,65 @@ def SEARCHLIMITED(S_nodes,search_region,G):
             break
     return embeddings
 
+def candidates(G, S_nodes, v, u):
+        # candidate embeddings to be checked which is generated from domains of MNI
+        embeddings = {frozenset({u})}
+        for i in (S_nodes-{v}):
+            newembeddings = set()
+            for j in fels_dict.domain(S_nodes)[i]:
+                for k in embeddings:
+                    union = k | {j}
+                    if nx.is_connected(G.subgraph(union)):
+                        newembeddings.add(union)
+            embeddings = newembeddings
+        return embeddings
+
+def exists_embeddings(G, S_nodes, v, u):
+    exists = False
+    frequent = False
+    cand = candidates(G, S_nodes, v, u)
+    for c in cand:
+        gm =  isomorphism.GraphMatcher(G.subgraph(S_nodes), G.subgraph(c))
+        for e in gm.isomorphisms_iter():
+            fels_dict.add(e)
+            if e[v] == u:
+                exists = True
+            if fels_dict.is_frequent(S_nodes, tau, G):
+                frequent = True
+                break
+        else:
+            continue
+        break
+    return exists, frequent
+
 def EVALUATE(G, tau, S_nodes):
     if not nx.is_connected(G.subgraph(S_nodes)):
         return False
-    components = nx.connected_components(G)
-    for cnodes in components:
-        gm = isomorphism.GraphMatcher(G.subgraph(cnodes),G.subgraph(S_nodes))
-        for i in gm.subgraph_isomorphisms_iter():
-            fels_dict.add(i)
-            if fels_dict.is_frequent(S_nodes, tau, G):
-                return True
-    return False
+    ds = direct_subgraphs(G, S_nodes)
+    fels_dict.intersection(S_nodes, ds, G)
+    for v in S_nodes:
+        count = 0
+        if fels_dict.is_infrequent(S_nodes, tau):
+            return False
+        for u in fels_dict.domain(S_nodes)[v]:
+            if u in fels_dict.mni(S_nodes)[v]:
+                count += 1
+            else:
+                exists, frequent = exists_embeddings(G, S_nodes, v, u)
+                if frequent:
+                    return True
+                if exists:
+                    count += 1
+                else:
+                    fels_dict.remove(S_nodes, v, u)
+            if count == tau:
+                break
+        else:
+            return False
+    if fels_dict.is_frequent(S_nodes, tau, G) == False:
+        print(fels_dict.mni(S_nodes))
+    assert fels_dict.is_frequent(S_nodes, tau, G) == True
+    return True
 
 def UPDATEFRINGE(fringe, S_nodes, isFreq, tau, G):
     deleted = False
@@ -142,7 +221,7 @@ def UPDATEFRINGE(fringe, S_nodes, isFreq, tau, G):
             MFSi = fringe.MFS[i]
             if len(MFSi) == len(S_nodes) and MFSi != S_nodes:
                 u = MFSi | S_nodes
-                if u not in fringe.MIFS:
+                if u not in fringe.MIFS and u not in fringe.MFS:
                     if not EVALUATE(G,tau,u):
                         joined = fringe.addMIFS(u)
     return deleted
@@ -154,14 +233,29 @@ def incGM_plus(G, fringe, tau, newgraph):
     newnodes = frozenset(newgraph.nodes)
     fringe.addMIFS(newnodes)
     i = 0
+    sl = 0
+    ev = 0
     while 0 <= i <len(fringe.MIFS):
         S_nodes = fringe.MIFS[i]
+        sl_tik = time.time()
         embeds = SEARCHLIMITED(S_nodes, newnodes,G)
+        sl_tok = time.time()
+        sl += sl_tok - sl_tik
+        print("search limited:", sl)
         if not embeds:
+            ev_tik = time.time()
             isFreq = EVALUATE(G,tau,S_nodes)
+            ev_tok = time.time()
+            ev += ev_tok - ev_tik
+            print("evaluate:",ev)
         else:
             isFreq = fels_dict.is_frequent(S_nodes, tau, G)
+
+        ev_tik = time.time()
         delete = UPDATEFRINGE(fringe, S_nodes, isFreq, tau, G)
+        ev_tok = time.time()
+        ev += ev_tok - ev_tik
+        print("evaluate:",ev)
         i = i + 1 - int(delete)
     return fringe.MFS
 
@@ -178,7 +272,10 @@ for e in base.edges:
     tik = time.time()
     incGM_plus(G,fringe,tau,base.subgraph(e))
     tok = time.time()
-    print(tok-tik)
+    print("TOTAL:",tok-tik)
+    print("=================")
+    print()
+    print()
 distinct = [i for i in fringe.MFS]
 
 for i in range(len(distinct)-1):
